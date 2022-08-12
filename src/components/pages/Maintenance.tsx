6942e2d2496daf3f1aa101fe4d65f0461ee7eba7
Ebonian <tanadon.santisan@gmail.com>
2022-08-12 18:23:33 +0700

    feat: responsive maintenance page

--- src/components/pages/Maintenance.tsx
+++ src/components/pages/Maintenance.tsx
@@ -2,11 +2,15 @@ import { FiGithub, FiInstagram, FiMail } from "react-icons/fi";
 
 const Maintenance: React.FC = () => {
   return (
-    <div className="grid place-content-center min-h-screen bg-[#121212] text-white font-code">
-      <h1 className="font-semibold text-5xl">{"👨‍💻 Site under maintenance"}</h1>
-      <div className="ml-24 mt-1.5 space-y-10">
+    <div className="grid place-content-center min-h-screen bg-[#121212] text-white font-code p-10">
+      <span className="sm:hidden text-6xl mb-3 -ml-2">👨‍💻</span>
+      <h1 className="font-semibold text-4xl md:text-5xl">
+        <span className="hidden sm:inline">👨‍💻</span>
+        {" Site under maintenance"}
+      </h1>
+      <div className="sm:ml-[4.5rem] md:ml-24 mt-3 sm:mt-1.5 space-y-10">
         <p>Will be right back very soon ⚡</p>
-        <div className="flex items-center space-x-8">
+        <div className="flex items-center space-x-8 select-none">
           <a
             href="https://github.com/Ebonian"
             target="_blank"

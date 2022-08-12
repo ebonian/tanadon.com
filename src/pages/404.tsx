c9ccc0aaf0f8d3b4b47720f0db15eaf500003157
Ebonian <tanadon.santisan@gmail.com>
2022-08-12 23:18:03 +0700

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Maintenance from "../components/pages/Maintenance";
+
+const NotFound: NextPage = () => {
+  return <Maintenance />;
+};
+
+export default NotFound;

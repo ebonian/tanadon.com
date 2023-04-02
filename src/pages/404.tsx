bf69aeac636b411bab4ec6a7cb14a8a098c44a52
Ebonian <tanadon.santisan@gmail.com>
2023-04-02 09:34:15 +0700

    rebase

--- src/pages/404.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import Maintenance from "../components/pages/Maintenance";
-
-const NotFound: NextPage = () => {
-  return <Maintenance />;
-};
-
-export default NotFound;
--- src/pages/me.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const Me: NextPage = () => {
-  return <div>Me</div>;
-};
-
-export default Me;

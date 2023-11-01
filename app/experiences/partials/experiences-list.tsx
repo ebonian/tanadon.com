996ec7e6efea28bcdcb15fe696da26dfc8c093e3
ebonian <tanadon.santisan@gmail.com>
2023-11-01 17:13:01 +0700

    feat: qr

--- app/experiences/partials/experiences-list.tsx
+++ app/experiences/partials/experiences-list.tsx
@@ -1,14 +1,18 @@
+import Link from 'next/link';
+import { FiArrowRight } from 'react-icons/fi';
+
 import experiences from '@/data/experiences.json';
 
 export default function ExperiencesList() {
     return (
         <div className="grid w-full gap-5">
             {experiences.map((experience, index) => (
-                <div
-                    className="relative h-48 overflow-hidden rounded-[48px] bg-white/10"
+                <Link
+                    href={`/projects?organization=${experience.slug}`}
+                    className="group relative h-48 overflow-hidden rounded-[48px] bg-white/10"
                     key={index}
                 >
-                    <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center px-14">
+                    <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-between space-x-6 px-10 lg:px-14">
                         <div>
                             <h2 className="text-2xl font-semibold">
                                 {experience.organization}
@@ -17,11 +21,17 @@ export default function ExperiencesList() {
                                 {experience.position}
                             </p>
                         </div>
+                        <div className="text-3xl2 lg:hidden">
+                            <FiArrowRight />
+                        </div>
                     </div>
-                    <p className="absolute -bottom-6 -right-2.5 z-10 select-none text-9xl font-bold text-white/5">
+                    <p className="absolute -bottom-6 -right-2.5 z-10 hidden select-none bg-gradient-to-r from-[#c3e4ff] to-[#b9beff] bg-clip-text text-9xl font-bold text-transparent opacity-0 duration-1000 ease-out group-hover:opacity-100 lg:inline">
                         {experience.year}
                     </p>
-                </div>
+                    <p className="absolute -bottom-6 -right-2.5 z-10 select-none text-9xl font-bold text-white/[2%] opacity-100 duration-1000 ease-out lg:text-white/[3%] lg:group-hover:opacity-0">
+                        {experience.year}
+                    </p>
+                </Link>
             ))}
         </div>
     );
--- app/tools/qr/page.tsx
+++ app/tools/qr/page.tsx
@@ -1,3 +1,39 @@
-export default function QR() {
-    return <div>QR</div>;
+'use client';
+
+import QRCode from 'qrcode';
+import { useState } from 'react';
+
+export default function QR(): JSX.Element {
+    const [url, setUrl] = useState('');
+    const [qr, setQr] = useState('');
+
+    const GenerateQRCode = () => {
+        if (url) {
+            QRCode.toDataURL(
+                url,
+                {
+                    width: 500,
+                    margin: 2,
+                    color: {
+                        dark: '#121212',
+                        light: '#ffffff',
+                    },
+                },
+                (err, url) => {
+                    if (err) return console.error(err);
+
+                    setQr(url);
+                },
+            );
+        } else {
+            setQr('');
+        }
+    };
+
+    return (
+        <div className="grid grid-cols-2">
+            <div className="h-full w-full bg-white/10"></div>
+            <div className="h-full w-full bg-white/10"></div>
+        </div>
+    );
 }
